4c15b77074f12e61d3b9f68de09c982ab9b01edd
Subadra bro <scienceposhitha@gmail.com>
2021-12-17 16:36:21 -0700

    {AiDarkEzio} Commited♻ (bug fix)

--- plugins/YouTube Video Downloder.js
+++ plugins/YouTube Video Downloder.js
@@ -28,7 +28,6 @@ const ExchangeRatesError = require('exchange-rates-api/src/exchange-rates-error.
 const fs = require('fs');
 const https = require('https');
 const googleTTS = require('google-translate-tts');
-//=====================================================================================
 //============================== YOUTUBE ==============================================
 const ytdl = require('ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
@@ -36,85 +35,14 @@ const yts = require( 'yt-search' )
 const got = require("got");
 const ID3Writer = require('browser-id3-writer');
 const SpotifyWebApi = require('spotify-web-api-node');
-
-const spotifyApi = new SpotifyWebApi({
-    clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3',
-    clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009'
-});
 //=====================================================================================
 const Language = require('../language');
 const Lang = Language.getString('scrapers');
-const Glang = Language.getString('github');
-const Slang = Language.getString('lyrics');
-const Clang = Language.getString('covid');
-
 const wiki = require('wikijs').default;
 var gis = require('g-i-s');
-
-var dlang_dsc = ''
-var closer_res = ''
-var dlang_lang = ''
-var dlang_similarity = ''
-var dlang_other = ''
-var dlang_input = ''
-
 const newLocal = "status@broadcast";
 
-Julie.addCommand({pattern: 'video ?(.*)', fromMe: true, desc: Lang.VIDEO_DESC}, (async (message, match) => { 
-
-    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_VIDEO,MessageType.text);    
-
-    var VID = '';
-    try {
-        if (match[1].includes('watch')) {
-            var tsts = match[1].replace('watch?v=', '')
-            var alal = tsts.split('/')[3]
-            VID = alal
-        } else {     
-            VID = match[1].split('/')[3]
-        }
-    } catch {
-        return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
-    }
-
-    var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_VIDEO,MessageType.text, {quoted : {
-        key: {
-          fromMe: false,
-          participant: message.jid,
-          remoteJid: newLocal
-        },
-        message: {
-          "extendedTextMessage": {
-            "text": "*Queen Jennifer*"
-          }
-        }
-    }
-    });
-
-    var yt = ytdl(VID, {filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p'].map(() => true)});
-
-    yt.pipe(fs.createWriteStream('./' + VID + '.mp4'));
-
-    yt.on('end', async () => {
-        reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-        await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4, caption: help.songsender(name,url,time,ago,views,cname) ,
-            quoted : {
-                key: { 
-                    fromMe: false, 
-                    participant: message.jid, 
-                    remoteJid: newLocal
-                }, message: { 
-                    "extendedTextMessage": { 
-                        "text": "*Queen Jennifer*" 
-                    }
-                }
-            }});
-    });
-}));
-
-if (config.WORKTYPE == 'public') {
-
-    Julie.addCommand({pattern: 'video ?(.*)', fromMe: false, desc: Lang.VIDEO_DESC}, (async (message, match) => { 
+Julie.addCommand({pattern: 'video ?(.*)', fromMe: false, desc: Lang.VIDEO_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_VIDEO,MessageType.text);    
     
@@ -130,40 +58,24 @@ if (config.WORKTYPE == 'public') {
         } catch {
             return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
         }
-    
         var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_VIDEO,MessageType.text, {quoted : {
             key: {
-              fromMe: false,
-              participant: message.jid,
-              remoteJid: newLocal
+              fromMe: true,
+              participant: "0@s.whatsapp.net",
+              remoteJid: "status@broadcast"
             },
             message: {
               "extendedTextMessage": {
-                "text": "*Queen Jennifer*"
+                "text": "*Dowloading*"
               }
             }
         }
         });
-        
         var yt = ytdl(VID, {filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p'].map(() => true)});
-    
         yt.pipe(fs.createWriteStream('./' + VID + '.mp4'));
-    
+
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4, caption: help.songsender(name,url,time,ago,views,cname) ,
-                quoted : {
-                    key: { 
-                        fromMe: false, 
-                        participant: message.jid, 
-                        remoteJid: newLocal
-                    }, message: { 
-                        "extendedTextMessage": { 
-                            "text": "*Queen Jennifer*" 
-                        }
-                    }
-                }});
+            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4});
         });
     }));
-
-}
